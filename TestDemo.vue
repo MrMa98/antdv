f73b213cdc3f2ed8bbfe693b99162d9ba247a16d
Mr.Ma <2192179354@qq.com>
2024-02-21 17:32:13 +0800

    在第一层抽屉中打开内部抽屉

---
+++ TestDemo.vue
@@ -0,0 +1,40 @@
+<template>
+    <a-button type="primary" @click="showDrawer">Open</a-button>
+    <a-drawer
+      v-model:open="open"
+      title="Multi-level drawer"
+      width="520"
+      :closable="false"
+      :footer-style="{ textAlign: 'right' }"
+      @close="onClose"
+      :style="{ position: 'relative' }"
+    >
+      <a-button type="primary" @click="showChildrenDrawer">Two-level drawer</a-button>
+      <a-drawer v-model:open="childrenDrawer" title="Two-level Drawer" width="320" :closable="false" :get-container="false"
+                    :style="{ position: 'absolute' }" >
+        <a-button type="primary" @click="showChildrenDrawer">This is two-level drawer</a-button>
+      </a-drawer>
+  
+      <template #footer>
+        <a-button style="margin-right: 8px" @click="onClose">Cancel</a-button>
+        <a-button type="primary" @click="onClose">Submit</a-button>
+      </template>
+    </a-drawer>
+  </template>
+  <script lang="ts" setup>
+  import { ref } from 'vue';
+  
+  const open = ref<boolean>(false);
+  
+  const childrenDrawer = ref<boolean>(false);
+  
+  const showDrawer = () => {
+    open.value = true;
+  };
+  const onClose = () => {
+    open.value = false;
+  };
+  const showChildrenDrawer = () => {
+    childrenDrawer.value = true;
+  };
+  </script>
